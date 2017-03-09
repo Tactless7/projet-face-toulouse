291f1e71e1aa283f44f615e13c5fc50ad662d3a6
Juliane Blier <blier.juliane@gmail.com>
2017-03-09 09:49:19 +0100

    Création du Model Member

--- plugins/tactless/members/lang/fr/lang.php
+++ plugins/tactless/members/lang/fr/lang.php
@@ -1,6 +1,12 @@
 <?php return [
     'plugin' => [
         'name' => 'Members',
-        'description' => 'A plugin to display members of a team or an organization'
-    ]
+        'description' => 'A plugin to display members of a team or an organization',
+        'first_name' => 'Prénom',
+        'last_name' => 'Nom',
+        'job' => 'Fonction au sein de la structure',
+        'role' => 'Rôle',
+        'phone' => 'Téléphone',
+        'email' => 'Email',
+    ],
 ];
---
+++ plugins/tactless/members/models/Member.php
@@ -0,0 +1,26 @@
+<?php namespace Tactless\Members\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Member extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    use \October\Rain\Database\Traits\SoftDelete;
+
+    protected $dates = ['deleted_at'];
+
+    /*
+     * Validation
+     */
+    public $rules = [
+    ];
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'tactless_members_';
+}
