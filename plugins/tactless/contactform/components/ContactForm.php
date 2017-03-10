129ab71d98a3838eae43f8653c1528f1c51f8670
Juliane Blier <blier.juliane@gmail.com>
2017-03-10 16:28:25 +0100

    Ajout du component formulaire

---
+++ plugins/tactless/contactform/components/ContactForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tactless\Contactform\Components;
+
+use Cms\Classes\ComponentBase;
+
+class ContactForm extends ComponentBase {
+
+    public function componentDetails(){
+        return [
+            'name' => 'Contact Form',
+            'description' => 'A simple contact form',
+        ];
+    }
+
+}
